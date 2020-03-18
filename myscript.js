166dc6a1f215ceb5706a940182a1c3dc0adf4545
Alessandro <alecritelli@gmail.com>
2020-03-18 23:23:51 +0100

    first js script

---
+++ myscript.js
@@ -0,0 +1,22 @@
+
+// Chiedi all’utente il suo nome,
+var input_name = prompt("Ciao Frenzo come ti chiami?");
+
+// scrivi il risultato del form sulla pagina html
+document.getElementById('input_name').innerHTML = input_name
+
+//  chiedi il suo cognome,
+var input_surname = prompt("Qual'è il tuo cognome?");
+
+// scrivi il risultato del form sulla pagina html
+document.getElementById('input_surname').innerHTML = input_surname
+
+// poi chiedi il suo colore preferito
+var input_color = prompt("L'interrogatorio finisce qui, qual'è il tuo colore preferito?")
+
+// scrivi il risultato del form sulla pagina html
+document.getElementById('input_color').innerHTML = input_color
+
+
+// Infine scrivi sulla pagina nomecognomecolorepreferito19
+document.getElementById('input_color').innerHTML = input_name+input_surname+input_color
