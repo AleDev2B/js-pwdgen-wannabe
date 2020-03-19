9a12f6650b8efac235dbf75d4c08206eb9231212
Alessandro <alecritelli@gmail.com>
2020-03-19 22:23:12 +0100

    italian fix

--- myscript.js
+++ myscript.js
@@ -6,13 +6,13 @@ var input_name = prompt("Ciao Frenzo come ti chiami?");
 document.getElementById('input_name').innerHTML = input_name
 
 //  chiedi il suo cognome,
-var input_surname = prompt("Qual'è il tuo cognome?");
+var input_surname = prompt("Qual è il tuo cognome?");
 
 // scrivi il risultato del form sulla pagina html
 document.getElementById('input_surname').innerHTML = input_surname
 
 // poi chiedi il suo colore preferito
-var input_color = prompt("L'interrogatorio finisce qui, qual'è il tuo colore preferito?")
+var input_color = prompt("L'interrogatorio finisce qui, qual è il tuo colore preferito?")
 
 // scrivi il risultato del form sulla pagina html
 document.getElementById('input_color').innerHTML = input_color
